3f705d428d9735caeebc84190f209a6e5236c24b
hannahoriel <hannahoriel07@gmail.com>
2026-09-12 10:22:19 +0800

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Admin\Admin;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,11 +16,43 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $admin = [
+            'first_name' => 'Shop',
+            'middle_name' => null,
+            'last_name' => 'Ease',
+            'phone_number' => '0912345678',
+            'email' => 'admin@gmail.com',
+            'password' => 'Admin@123',
+            'profile_picture' => null,
+        ];
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Admin::updateOrCreate(
+            ['email' => $admin['email']],
+            $admin
+        );
+
+        User::updateOrCreate(
+            ['email' => $admin['email']],
+            [
+                'name' => 'Shop Ease',
+                'first_name' => $admin['first_name'],
+                'last_name' => $admin['last_name'],
+                'contact_no' => $admin['phone_number'],
+                'role' => User::ROLE_ADMIN,
+                'password' => $admin['password'],
+            ]
+        );
+
+        foreach ([
+            ['name' => 'Buyer User', 'email' => 'buyer@shopease.test', 'role' => User::ROLE_BUYER, 'password' => 'password'],
+            ['name' => 'Seller User', 'email' => 'seller@shopease.test', 'role' => User::ROLE_SELLER, 'password' => 'seller123'],
+            ['name' => 'Logistics User', 'email' => 'logistics@shopease.test', 'role' => User::ROLE_LOGISTICS, 'password' => 'password'],
+            ['name' => 'Rider User', 'email' => 'rider@shopease.test', 'role' => User::ROLE_RIDER, 'password' => 'password'],
+        ] as $account) {
+            User::updateOrCreate(
+                ['email' => $account['email']],
+                $account
+            );
+        }
     }
 }
